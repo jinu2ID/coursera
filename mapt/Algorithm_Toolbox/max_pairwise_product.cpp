6c367dee8878248b2e3a1bb671b898244c916dcc
Jinu <jinu.jacob90@gmail.com>
2016-10-18 15:00:53 -0400

    added stress test; fixed bug in max_pairwise_product

--- mapt/Algorithm_Toolbox/max_pairwise_product.cpp
+++ mapt/Algorithm_Toolbox/max_pairwise_product.cpp
@@ -6,51 +6,24 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-void swap(long long* a, long long* b) {
-  long long t = *a;
-  *a = *b;
-  *b = t;
-}
-
-long partition(long long arr[], long low, long high) {
-  long long pivot = arr[high];
-  long i = (low - 1);
+long long MaxPairwiseProduct(vector<long long>& numbers) {
+  long n = numbers.size();
   
-  for (long j = low; j <= high-1; j++){
-    if (arr[j] <= pivot){
-      i++;
-      swap(&arr[i], &arr[j]);
+  long max_index1 = -1;
+  for (long i = 0; i < n; ++i) {
+    if ((max_index1 == -1) || (numbers[i] > numbers[max_index1])) {
+      max_index1 = i;
     }
   }
 
-  swap(&arr[i+1], &arr[high]);
-  return (i+1);
-}
-
-void quickSort(long long arr[], long low, long high) {
-  if (low < high) {
-    // partition index
-    long pi = partition(arr, low, high);
-
-    quickSort(arr, low, pi-1);
-    quickSort(arr, pi+1, high);
-  }  
-
-}
-
-long long MaxPairwiseProduct(const vector<long long>& numbers) {
-  long long result = 0;
-  long long n = numbers.size();
-  for (long long i = 0; i < n; ++i) {
-    for (long long j = i + 1; j < n; ++j) {
-      if (numbers[i] * numbers[j] > result) {
-//        cout << numbers[i] << " " << numbers[j] << endl;
-        result = numbers[i] * numbers[j];
-//        cout << result << endl;
-      }
+  long max_index2 = -1;
+  for (long j = 0; j < n; ++j) {
+    if ((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2]))) {
+      max_index2 = j;
     }
   }
-  return result;
+
+  return ((long long)(numbers[max_index1])) * numbers[max_index2];
 }
 
 int main() {
