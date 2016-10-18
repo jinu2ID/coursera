5458336ec444a893e6469d4e8fc67590412d8fff
Jinu <jinu.jacob90@gmail.com>
2016-10-18 12:36:40 -0400

    added .gitignore

--- mapt/Algorithm_Toolbox/max_pairwise_product.cpp
+++ mapt/Algorithm_Toolbox/max_pairwise_product.cpp
@@ -6,6 +6,38 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+void swap(long long* a, long long* b) {
+  long long t = *a;
+  *a = *b;
+  *b = t;
+}
+
+long partition(long long arr[], long low, long high) {
+  long long pivot = arr[high];
+  long i = (low - 1);
+  
+  for (long j = low; j <= high-1; j++){
+    if (arr[j] <= pivot){
+      i++;
+      swap(&arr[i], &arr[j]);
+    }
+  }
+
+  swap(&arr[i+1], &arr[high]);
+  return (i+1);
+}
+
+void quickSort(long long arr[], long low, long high) {
+  if (low < high) {
+    // partition index
+    long pi = partition(arr, low, high);
+
+    quickSort(arr, low, pi-1);
+    quickSort(arr, pi+1, high);
+  }  
+
+}
+
 long long MaxPairwiseProduct(const vector<long long>& numbers) {
   long long result = 0;
   long long n = numbers.size();
