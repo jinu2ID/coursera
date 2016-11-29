14da5b7e5ce8d254ad85f713a781964dca1bf43f
Jinu <jinu.jacob90@gmail.com>
2016-11-28 20:44:51 -0500

    finished change and fractional_knapsack problems

--- mapt/Algorithm_Toolbox/03_greedy_algorithms/fractional_knapsack/fractional_knapsack.cpp
+++ mapt/Algorithm_Toolbox/03_greedy_algorithms/fractional_knapsack/fractional_knapsack.cpp
@@ -1,29 +1,94 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
+using std::swap;
+using std::cout;
+using std::endl;
+using std::setprecision;
+using std::fixed;
 
-double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
+void printVectors(vector<double> values, vector<double> weights)
+{
+  for (int i = 0; i < values.size(); i++) {
+    cout << values[i] << " " << weights[i] << endl;
+  }
+}
+
+int partition (vector<double> &values, vector<double> &weights, int low, int high) 
+{ 
+  double pivot = values[high]/weights[high];
+  int i = (low - 1);
+
+  for (int j = low; j <= high-1; j++)
+  {
+    double valuePerWeight = values[j]/weights[j];
+    if (valuePerWeight <= pivot)
+    {
+      // move wall 1 space to right, elements to the left are smaller than pivot, 
+      // elements to the right are larger than pivot
+      i++; 
+      swap(values[i], values[j]);
+      swap(weights[i], weights[j]);
+    }
+  }
+
+ swap(values[i+1], values[high]);
+ swap(weights[i+1], weights[high]);
+
+ return (i+1);
+  
+}
+
+void quickSort(vector<double> &values, vector<double> &weights, int low, int high)
+{
+  if (low < high) {
+    int pivot = partition(values, weights, low, high);
+    quickSort(values, weights, low, pivot-1);
+    quickSort(values, weights, pivot+1, high);
+  } 
+}
+
+
+
+double get_optimal_value(int n, long capacity, vector<double> values, vector<double> weights) {
   double value = 0.0;
+  quickSort(values, weights, 0, n-1);
+  
+  int i = n-1;
+  while (capacity > 0 && i >= 0) {
+    double amount = weights[i];
+    
+    if (amount > capacity) {
+      amount = capacity;
+    }
+  
+    capacity -= amount;  
+    value += amount*((double)values[i]/(double)weights[i]);
+    
+    i--;
 
-  // write your code here
-
+  }
   return value;
 }
 
+
+
 int main() {
   int n;
-  int capacity;
+  long capacity;
   std::cin >> n >> capacity;
-  vector<int> values(n);
-  vector<int> weights(n);
+  vector<double> values(n);
+  vector<double> weights(n);
   for (int i = 0; i < n; i++) {
     std::cin >> values[i] >> weights[i];
   }
-
-  double optimal_value = get_optimal_value(capacity, weights, values);
+  
+  double optimal_value = get_optimal_value(n, capacity, values, weights);
 
   std::cout.precision(10);
-  std::cout << optimal_value << std::endl;
+  std::cout << fixed << setprecision(4) << optimal_value << std::endl;
   return 0;
 }
